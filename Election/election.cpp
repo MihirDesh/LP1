7b7cfcee4e34874ccb1c55babe84d8fafc4f8be6
Mihir <34931141+MihirDesh@users.noreply.github.com>
2023-11-08 23:03:42 +0530

    added message feature in bully algorithm

--- Election/election.cpp
+++ Election/election.cpp
@@ -108,25 +108,45 @@ class Election
         int old = currindex;
         int a = rand() % (v.size() - 1);
         int new1 = currindex - a;
-        cout<<"Detected By: "<<v[currindex - a].id<<endl;
+        cout << "Detected By: " << v[currindex - a].id << endl;
         v[currindex].act = false;
         sleep(1);
-        cout<<"Election Initialized"<<endl;
-        while(true)
+        cout << "Election Initialized" << endl;
+    
+        while (true)
         {
-            for(int i = new1; i < v.size() - 1; i++)
+            // Send election message to higher processes
+            for (int i = new1; i < v.size() - 1; i++)
             {
-                if(v[i].act)
-                    cout<<"Election message from "<<v[new1].id<<" to "<<v[i + 1].id<<endl;
+                if (v[i].act)
+                {
+                    cout << "Election message from " << v[new1].id << " to " << v[i + 1].id << endl;
+                    // Process i+1 receives the message and responds, check if it wants to participate
+                    if (/* Condition to respond positively, e.g., if it's not participating yet */)
+                    {
+                        cout << "OK message from " << v[i + 1].id << " to " << v[new1].id << endl;
+                        // Process i+1 accepts the election and responds positively
+                    }
+                    else
+                    {
+                        // Process i+1 rejects the election, and you may want to inform others
+                    }
+                }
                 sleep(1);
             }
-            for(int i = v.size() - 1; i > new1; i--)
+    
+            // Send OK messages to lower processes
+            for (int i = v.size() - 1; i > new1; i--)
             {
-                if(v[i].act)
-                cout<<"OK message from "<<v[i].id<<" to "<<v[new1].id<<endl;
+                if (v[i].act)
+                {
+                    cout << "OK message from " << v[i].id << " to " << v[new1].id << endl;
+                    // Process new1 receives the OK message
+                }
                 sleep(1);
             }
-            if(new1 < v.size() - 1 && v[new1].act)
+    
+            if (new1 < v.size() - 1 && v[new1].act)
             {
                 new1 = new1 + 1;
             }
@@ -135,7 +155,8 @@ class Election
                 break;
             }
         }
-        cout<<"Elected Coordinator is: "<<v[new1 - 1].id<<endl;
+    
+        cout << "Elected Coordinator is: " << v[new1 - 1].id << endl;
     }
 };
 int main()
